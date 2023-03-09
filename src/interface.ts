05180720f526a99a09ded0dccce4239e87b68269
2yunseong <dbsdltjd3701@naver.com>
2023-03-09 22:11:37 +0900

    build 파일 배포

--- src/interface.ts
+++
@@ -1,6 +0,0 @@
-export interface Component<T> {
-  $target: HTMLElement;
-  state: T;
-  setState?: (state: T) => void;
-  render?: () => void;
-}
--- src/utils/util.js
+++
@@ -1,5 +0,0 @@
-import { STAR_FILL_IMAGE_URL, STAR_LINE_IMAGE_URL } from './constants';
-
-export const getImgSrcByFavorite = (isFavorite) => {
-  return isFavorite ? STAR_FILL_IMAGE_URL : STAR_LINE_IMAGE_URL;
-};
